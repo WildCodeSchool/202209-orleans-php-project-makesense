7d5948a33b6995fb50b86bb28e4aebb3dd268de4
GARCIA_STEPHANIE <45000steph@gmail.com>
2022-12-15 00:01:29 +0100

    titre décisions

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('home', './assets/styles/home.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
