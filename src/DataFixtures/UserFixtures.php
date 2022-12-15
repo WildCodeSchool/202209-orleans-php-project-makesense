b26a9304b49c41bb75dd1cf2cad5a13eb4d1449f
ByZirTox <xxtedyxxrenard@gmail.com>
2022-12-15 14:53:16 +0100

    Fixture finish

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $count = 0;
+        while ($count < 5) {
+            $faker = Factory::create();
+            $user = new User();
+            $user->setEmail($faker->email());
+            $user->setRoles([]);
+            $user->setPassword($faker->password());
+            $user->setFirstname($faker->firstName());
+            $user->setLastname($faker->lastName());
+            $user->setIsApproved('0');
+            $manager->persist($user);
+            $count++;
+        }
+        $manager->flush();
+    }
+}
