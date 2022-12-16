15a063e24936781a423f642636a33a7c1ef0bce3
ByZirTox <xxtedyxxrenard@gmail.com>
2022-12-16 15:26:23 +0100

    Save

--- src/Controller/ProfilUserController.php
+++ src/Controller/ProfilUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\DecisionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfilUserController extends AbstractController
 {
     #[Route('/profil/user', name: 'app_profil_user')]
-    public function index(): Response
+    public function index(DecisionRepository $decisionRepository): Response
     {
+
+        $decisions = $decisionRepository->findBy(
+            [],
+            ['decisionStartTime' => 'ASC'],
+            12
+        );
+
         return $this->render('profil_user/index.html.twig', [
             'controller_name' => 'ProfilUserController',
+            'decisions' => $decisions,
         ]);
     }
 }
