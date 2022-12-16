8338f64b91c79ec64feceac7ffc51c5083f89589
ByZirTox <xxtedyxxrenard@gmail.com>
2022-12-16 11:46:41 +0100

    save

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,25 +7,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class LoginController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_home');
-        }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
-
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
     }
 }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,9 +6,19 @@ use Faker\Factory;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
@@ -24,6 +34,35 @@ class UserFixtures extends Fixture
             $manager->persist($user);
             $count++;
         }
+
+
+        $user = new User();
+
+        $user->setEmail('tedyDoe@gmail.com');
+        $user->setRoles([]);
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            'tedytedy'
+        );
+        $user->setPassword($hashedPassword);
+        $user->setFirstname('Tedy');
+        $user->setLastname('Doe');
+        $user->setIsApproved('0');
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('admin@make-sence.ms');
+        $user->setRoles([]);
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            'adminadmin'
+        );
+        $user->setPassword($hashedPassword);
+        $user->setFirstname('Admin');
+        $user->setLastname('MakeSence');
+        $user->setIsApproved('0');
+        $manager->persist($user);
+
         $manager->flush();
     }
 }
