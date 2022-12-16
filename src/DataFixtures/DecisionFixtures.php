4bcd632bc2bf365afe32f49cbaeab348b4cee303
V-Gillet <gillet.valentin45000@gmail.com>
2022-12-16 11:35:52 +0100

    changed loop const name

--- src/DataFixtures/DecisionFixtures.php
+++ src/DataFixtures/DecisionFixtures.php
@@ -9,13 +9,13 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class DecisionFixtures extends Fixture
 {
-    public const LOOP_INDEX = 100;
+    public const LOOP_COUNT = 100;
 
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
         $index = 0;
-        while ($index < self::LOOP_INDEX) {
+        while ($index < self::LOOP_COUNT) {
             $decision = new Decision();
 
             $decision->setTitle($faker->sentence(rand(15, 45)));
