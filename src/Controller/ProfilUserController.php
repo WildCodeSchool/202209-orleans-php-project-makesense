cc9f14df8c1c95495299589dbd0b3f355dc611a2
ByZirTox <xxtedyxxrenard@gmail.com>
2022-12-21 17:12:44 +0100

    Review finished

--- src/Controller/ProfilUserController.php
+++ src/Controller/ProfilUserController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilUserController extends AbstractController
 {
-    public const DECISION_START_TIME = 12;
+    public const DECISION_LIMIT_NUMBER = 12;
 
     #[Route('/profil/user', name: 'app_profil_user')]
     public function index(DecisionRepository $decisionRepository): Response
@@ -18,7 +18,7 @@ class ProfilUserController extends AbstractController
         $decisions = $decisionRepository->findBy(
             [],
             ['decisionStartTime' => 'ASC'],
-            self::DECISION_START_TIME
+            self::DECISION_LIMIT_NUMBER
         );
 
         return $this->render('profil_user/index.html.twig', [
