341a9289ba1f3f019d6ee495b0a39d87e4d3e8d0
ByZirTox <xxtedyxxrenard@gmail.com>
2023-01-04 16:38:33 +0100

    Finish

--- migrations/Version20230104153537.php
+++ migrations/Version20230104153537.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221213162253 extends AbstractMigration
+final class Version20230104153537 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,14 @@ final class Version20221213162253 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE user ADD is_approved TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE decision DROP first_decision_end_date, DROP conflict_end_date, DROP final_decision_end_date');
+        $this->addSql('ALTER TABLE user ADD poster VARCHAR(255) NOT NULL, ADD updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE user');
-        $this->addSql('ALTER TABLE user DROP is_approved');
+        $this->addSql('ALTER TABLE decision ADD first_decision_end_date DATE DEFAULT NULL, ADD conflict_end_date DATE DEFAULT NULL, ADD final_decision_end_date DATE DEFAULT NULL');
+        $this->addSql('ALTER TABLE user DROP poster, DROP updated_at');
     }
 }
