8c84d6e5c89d8e5c4986a65e8539a42883645922
ByZirTox <xxtedyxxrenard@gmail.com>
2023-01-05 12:26:39 +0100

    Save

--- src/Controller/DecisionController.php
+++ src/Controller/DecisionController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Decision;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,12 +17,37 @@ class DecisionController extends AbstractController
     #[Route('decision/{decision}', methods: ['GET'], name: 'app_decision')]
     public function index(Decision $decision): Response
     {
-        return $this->render('decisions/decisionView.html.twig', ['decision' => $decision,]);
+        return $this->render('decisions/decisionView.html.twig', [
+            'decision' => $decision,
+        ]);
     }
 
+
     #[Route('decision/{decision}/avis', methods: ['GET'], name: 'app_decision_comment')]
-    public function comment(Decision $decision): Response
-    {
-        return $this->render('decisions/commentView.html.twig', ['decision' => $decision,]);
+    public function comment(
+        Decision $decision,
+        Request $request,
+        CommentRepository $commentRepository,
+    ): Response {
+
+
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $commentRepository->save($comment, true);
+        }
+        return $this->redirectToRoute('app_home');
+
+
+
+
+        return $this->render('decisions/commentView.html.twig', [
+            'decision' => $decision,
+            'commentForm' => $form,
+        ]);
     }
 }
---
+++ src/Form/CommentType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('comment')
+            ->add('commentTimedate')
+            ->add('isInConflict')
+            ->add('conflictColor')
+            ->add('commentColor')
+            ->add('interaction')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
