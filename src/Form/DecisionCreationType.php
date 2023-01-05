d4573e0b5cb3f99a000c8e91a9db22c6e766186c
V-Gillet <gillet.valentin45000@gmail.com>
2023-01-05 21:31:49 +0100

    added wysiwyg config and styled the inputs

--- src/Form/DecisionCreationType.php
+++ src/Form/DecisionCreationType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
 class DecisionCreationType extends AbstractType
