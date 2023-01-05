0e0ce50d62dd7457a35c13308824f6c2a17220e5
ByZirTox <xxtedyxxrenard@gmail.com>
2023-01-05 12:30:01 +0100

    Save

--- migrations/Version20230104153537.php
+++ migrations/Version20230104153537.php
@@ -22,6 +22,7 @@ final class Version20230104153537 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE decision DROP first_decision_end_date, DROP conflict_end_date, DROP final_decision_end_date');
         $this->addSql('ALTER TABLE user ADD poster VARCHAR(255) NOT NULL, ADD updated_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE poster poster VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
@@ -29,5 +30,6 @@ final class Version20230104153537 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE decision ADD first_decision_end_date DATE DEFAULT NULL, ADD conflict_end_date DATE DEFAULT NULL, ADD final_decision_end_date DATE DEFAULT NULL');
         $this->addSql('ALTER TABLE user DROP poster, DROP updated_at');
+        $this->addSql('ALTER TABLE user CHANGE poster poster VARCHAR(255) NOT NULL');
     }
 }
