9da9a3fc80fcc7a0a1902d153cd3a2ab41245466
V-Gillet <gillet.valentin45000@gmail.com>
2023-01-06 09:25:56 +0100

    put my profil in french

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -15,7 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[IsGranted('ROLE_USER')]
 class ProfileController extends AbstractController
 {
-    #[Route('/myprofile', name: 'app_view_my_profil')]
+    #[Route('/mon-profil', name: 'app_view_my_profil')]
     public function index(
         Request $request,
         UserRepository $userRepository,
