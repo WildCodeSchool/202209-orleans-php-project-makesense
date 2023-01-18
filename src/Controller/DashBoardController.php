07f99a5de80bf3c483439930cf638629ba0d1b4a
V-Gillet <gillet.valentin45000@gmail.com>
2023-01-18 13:32:12 +0100

    started style

---
+++ src/Controller/DashBoardController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use DateTime;
+use App\Form\DecisionSearchType;
+use App\Repository\DecisionRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[IsGranted('ROLE_USER')]
+class DashBoardController extends AbstractController
+{
+    #[Route('/dashboard/{user}', name: 'app_dashboard')]
+    public function index(DecisionRepository $decisionRepository, User $user): Response
+    {
+        $user = $this->getUser();
+
+        $myDecisions = $decisionRepository->findBy(['creator' => $user]);
+
+        return $this->render(
+            'dashboard/index.html.twig',
+            [
+                'myDecisions' => $myDecisions,
+            ],
+        );
+    }
+}
