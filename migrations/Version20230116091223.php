04ef01cfca91eee941066a3027ad616773cb690e
V-Gillet <gillet.valentin45000@gmail.com>
2023-01-18 15:57:58 +0100

    deleted unwanted  migration

--- migrations/Version20230116091223.php
+++ migrations/Version20230116091223.php
@@ -20,7 +20,7 @@ final class Version20230116091223 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE interaction (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, decision_id INT DEFAULT NULL, decision_role VARCHAR(45) DEFAULT NULL, INDEX IDX_378DFDA7A76ED395 (user_id), INDEX IDX_378DFDA7BDEE7539 (decision_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE interaction (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, decision_id INT DEFAULT NULL, decision_role VARCHAR(100) DEFAULT NULL, INDEX IDX_378DFDA7A76ED395 (user_id), INDEX IDX_378DFDA7BDEE7539 (decision_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE interaction ADD CONSTRAINT FK_378DFDA7A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE interaction ADD CONSTRAINT FK_378DFDA7BDEE7539 FOREIGN KEY (decision_id) REFERENCES decision (id)');
     }
