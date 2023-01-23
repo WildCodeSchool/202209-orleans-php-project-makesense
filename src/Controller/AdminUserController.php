4d8963f9361353b71add8965de240733f79545e2
ByZirTox <xxtedyxxrenard@gmail.com>
2023-01-23 12:23:15 +0100

    Save

---
+++ src/Controller/AdminUserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use DateTimeImmutable;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/admin/user')]
+class AdminUserController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('admin_user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+
+    #[Route('/{id}', name: 'app_admin_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('admin_user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    {
+
+        $form = $this->createForm(UserType::class, $user);
+        if ($form->getData() === 'ROLE_USER') {
+            $user->setRoles(["ROLE_USER"]);
+        } else {
+            $user->setRoles(["ROLE_ADMIN"]);
+        }
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+
+            return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $userRepository->remove($user, true);
+        }
+
+        return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
