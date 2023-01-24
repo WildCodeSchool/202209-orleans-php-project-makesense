60dfefff66b79cad5881eed1f4367ee843307606
ByZirTox <xxtedyxxrenard@gmail.com>
2023-01-24 17:29:23 +0100

    Save

--- migrations/Version20230116091223.php
+++ migrations/Version20230116091223.php
@@ -23,6 +23,10 @@ final class Version20230116091223 extends AbstractMigration
         $this->addSql('CREATE TABLE interaction (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, decision_id INT DEFAULT NULL, decision_role VARCHAR(100) DEFAULT NULL, INDEX IDX_378DFDA7A76ED395 (user_id), INDEX IDX_378DFDA7BDEE7539 (decision_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE interaction ADD CONSTRAINT FK_378DFDA7A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE interaction ADD CONSTRAINT FK_378DFDA7BDEE7539 FOREIGN KEY (decision_id) REFERENCES decision (id)');
+        $this->addSql('ALTER TABLE decision CHANGE creator_id creator_id INT NOT NULL');
+        $this->addSql('ALTER TABLE decision DROP FOREIGN KEY FK_84ACBE4861220EA6');
+        $this->addSql('ALTER TABLE interaction DROP FOREIGN KEY FK_378DFDA7A76ED395');
+        $this->addSql('ALTER TABLE interaction ADD CONSTRAINT FK_378DFDA7A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
@@ -31,5 +35,9 @@ final class Version20230116091223 extends AbstractMigration
         $this->addSql('ALTER TABLE interaction DROP FOREIGN KEY FK_378DFDA7A76ED395');
         $this->addSql('ALTER TABLE interaction DROP FOREIGN KEY FK_378DFDA7BDEE7539');
         $this->addSql('DROP TABLE interaction');
+        $this->addSql('ALTER TABLE decision CHANGE creator_id creator_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE decision ADD CONSTRAINT FK_84ACBE4861220EA661220EA6 FOREIGN KEY (creator_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE interaction DROP FOREIGN KEY FK_378DFDA7A76ED395');
+        $this->addSql('ALTER TABLE interaction ADD CONSTRAINT FK_378DFDA7A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE CASCADE');
     }
 }
