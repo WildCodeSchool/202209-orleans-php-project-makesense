ec5bcab6ae85cb0d2c6120ce50fb5615990d2883
ByZirTox <xxtedyxxrenard@gmail.com>
2023-01-27 15:44:11 +0100

    Finish AdminDecision for DELETE and SHOW

--- src/Controller/AdminDecisionController.php
+++ src/Controller/AdminDecisionController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Decision;
-use App\Form\DecisionType;
 use App\Repository\DecisionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,48 +20,11 @@ class AdminDecisionController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_admin_decision_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, DecisionRepository $decisionRepository): Response
-    {
-        $decision = new Decision();
-        $form = $this->createForm(DecisionType::class, $decision);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $decisionRepository->save($decision, true);
-
-            return $this->redirectToRoute('app_admin_decision_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('admin_decision/new.html.twig', [
-            'decision' => $decision,
-            'form' => $form,
-        ]);
-    }
-
     #[Route('/{id}', name: 'app_admin_decision_show', methods: ['GET'])]
     public function show(Decision $decision): Response
     {
         return $this->render('admin_decision/show.html.twig', [
             'decision' => $decision,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_admin_decision_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Decision $decision, DecisionRepository $decisionRepository): Response
-    {
-        $form = $this->createForm(DecisionType::class, $decision);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $decisionRepository->save($decision, true);
-
-            return $this->redirectToRoute('app_admin_decision_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('admin_decision/edit.html.twig', [
-            'decision' => $decision,
-            'form' => $form,
         ]);
     }
 
