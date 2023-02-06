48ddf9d4bbbf5563a68356fc1eb4158077691dfe
V-Gillet <gillet.valentin45000@gmail.com>
2023-02-06 09:36:11 +0100

    Fix: deleted useless migration

--- migrations/Version20230205215532.php
+++ migrations/Version20230205215532.php
@@ -22,7 +22,6 @@ final class Version20230205215532 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, decision_id INT DEFAULT NULL, decision_status VARCHAR(30) DEFAULT NULL, status_color CHAR(7) DEFAULT NULL, status_days_left VARCHAR(100) DEFAULT NULL, UNIQUE INDEX UNIQ_7B00651CBDEE7539 (decision_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE status ADD CONSTRAINT FK_7B00651CBDEE7539 FOREIGN KEY (decision_id) REFERENCES decision (id)');
-        $this->addSql('ALTER TABLE decision DROP decision_status, DROP status_color, DROP status_days_left');
     }
 
     public function down(Schema $schema): void
@@ -30,6 +29,5 @@ final class Version20230205215532 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE status DROP FOREIGN KEY FK_7B00651CBDEE7539');
         $this->addSql('DROP TABLE status');
-        $this->addSql('ALTER TABLE decision ADD decision_status VARCHAR(30) DEFAULT NULL, ADD status_color CHAR(7) DEFAULT NULL, ADD status_days_left VARCHAR(100) DEFAULT NULL');
     }
 }
